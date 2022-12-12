7a2814129908ded0f4bbaea4735e171eec241e3e
Worlax <g.worlax@gmail.com>
2022-12-12 20:00:44 +0600

    Toggle image class was reworked

---
+++ Assets/Project/UI/Editor/ToggleImageEditor.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(ToggleImage))]
+[CanEditMultipleObjects]
+public class ToggleImageEditor : Editor
+{
+	SerializedProperty useSprites;
+
+	// Sprites
+	new SerializedProperty target;
+	SerializedProperty spriteOn;
+	SerializedProperty spriteOff;
+
+	// Images
+	SerializedProperty imageOn;
+	SerializedProperty ImageOff;
+
+	// Unity
+	private void OnEnable()
+	{
+		useSprites = serializedObject.FindProperty("useSprites");
+
+		target = serializedObject.FindProperty("target");
+		spriteOn = serializedObject.FindProperty("spriteOn");
+		spriteOff = serializedObject.FindProperty("spriteOff");
+
+		imageOn = serializedObject.FindProperty("imageOn");
+		ImageOff = serializedObject.FindProperty("ImageOff");
+	}
+
+	public override void OnInspectorGUI()
+	{
+		EditorGUILayout.PropertyField(useSprites);
+
+		if (useSprites.boolValue)
+		{
+			EditorGUILayout.PropertyField(target);
+			EditorGUILayout.PropertyField(spriteOn);
+			EditorGUILayout.PropertyField(spriteOff);
+		}
+		else
+		{
+			EditorGUILayout.PropertyField(imageOn);
+			EditorGUILayout.PropertyField(ImageOff);
+		}
+
+		serializedObject.ApplyModifiedProperties();
+	}
+}
---
+++ Assets/Project/UI/ToggleImage.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+// Inspector preview is extended in "ToggleImageEditor"
+[RequireComponent(typeof(Toggle))]
+public class ToggleImage : MonoBehaviour
+{
+	[SerializeField] bool useSprites = true;
+
+	// Sprites
+	[SerializeField] Image target;
+	[SerializeField] Sprite spriteOn;
+	[SerializeField] Sprite spriteOff;
+
+	// Images
+	[SerializeField] Image imageOn;
+	[SerializeField] Image ImageOff;
+
+	// Unity
+	private void Start()
+	{
+		Toggle toggle = GetComponent<Toggle>();
+		toggle.onValueChanged.AddListener(ValueChanged);
+		ValueChanged(toggle.isOn);
+	}
+
+	// Events
+	void ValueChanged(bool value)
+	{
+		if (useSprites)
+		{
+			target.sprite = value ? spriteOn : spriteOff;
+		}
+		else
+		{
+			imageOn.gameObject.SetActive(value);
+			ImageOff.gameObject.SetActive(!value);
+		}
+	}
+}
